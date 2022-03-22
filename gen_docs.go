7298687c26dc6639b38e777436ee72707b5dec1a
John Hession <johhess@cdw.com>
2022-03-22 16:38:30 -0700

    Adding in outdir option for gendocs for WriteMarkdownTerra

--- gen_docs.go
+++ gen_docs.go
@@ -8,24 +8,43 @@ import (
 
 //WriteMarkdownTerra generates a README which recursively documents all terraform code
 //WriteMarkdownTerra does template execution to write the file
-func WriteMarkdownTerra(w Stats) os.File {
+func WriteMarkdownTerra(w Stats, o string) os.File {
+	var f *os.File
 	tmpls := template.New("md")
 	template.Must(tmpls.Parse(TerraDocGeneric))
 
-	f, err := os.Create(fmt.Sprintf("TERRAFORM.%s", tmpls.Name()))
-	if err != nil {
+	if len(o) > 0 {
+		f, err := os.Create(fmt.Sprintf("%s/TERRAFORM.%s", o, tmpls.Name()))
 		if err != nil {
-			fmt.Println("Unable to create file for Markdown")
+			if err != nil {
+				fmt.Printf("%s", err.Error())
+				os.Exit(1)
+			}
+		}
+
+		err = tmpls.Execute(f, w)
+		if err != nil {
+			fmt.Println(err.Error())
 			os.Exit(1)
 		}
-	}
+		return *f
+	} else {
+		f, err := os.Create(fmt.Sprintf("TERRAFORM.%s", tmpls.Name()))
+		if err != nil {
+			if err != nil {
+				fmt.Printf("%s", err.Error())
+				os.Exit(1)
+			}
+		}
 
-	err = tmpls.Execute(f, w)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+		err = tmpls.Execute(f, w)
+		if err != nil {
+			fmt.Printf("%s", err.Error())
+			os.Exit(1)
+		}
 
+		return *f
+	}
 	return *f
 }
 
@@ -76,6 +95,7 @@ func WriteMarkdownTfcState(st TfcState) os.File {
 
 	return *f
 }
+
 //WriteMarkdownRepo generates a README which recursively documents statistics about the repo from the root down
 //WriteMarkdownRepo does template execution to write the file
 func WriteMarkdownRepo(w RepoInfo) os.File {
